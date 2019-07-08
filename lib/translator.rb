5da404f90b539d33ef1168aec5f4133a0d93a308
Jeremy Hamilton <jeremyhamilton035@gmail.com>
2019-07-08 22:47:41 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -2,7 +2,9 @@
 def load_library(emoticons)
   require "yaml"
   emoticons = YAML.load_file('lib/emoticons.yml')
- 
+  data.each do |key, value|
+    value.each do |new_value, names|
+      names.each do |name|
 emoticons
 end
 
