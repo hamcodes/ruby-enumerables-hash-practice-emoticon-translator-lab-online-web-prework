9ee86e837b06f00aafc9087eda807e90d4a95a0b
Jeremy Hamilton <jeremyhamilton035@gmail.com>
2019-07-08 22:55:45 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -2,7 +2,18 @@
 def load_library(emoticons)
   require "yaml"
   emoticons = YAML.load_file('lib/emoticons.yml')
+  new_hash = {
+    'get_meaning' => {},
+    'get_emoticon' => {}
+  }
 
+  emoticons.each do |key,value|
+    new_hash['get_emoticon'][value[0]] = emoticons[key][1]
+    new_hash['get_meaning'][value[1]] = key
+  end
+
+  new_hash
+end
 end
 
 def get_japanese_emoticon
